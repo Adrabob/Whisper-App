a8d1f6642f55128697ee8344893bec6b97876c34
Adrabob <arda_kaya.fb@hotmail.com>
2023-07-12 17:33:10 +0300

    Files Updated
    Signed-off-by: Adrabob <arda_kaya.fb@hotmail.com>

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const bodyParser = require("body-parser");
 const ejs =require("ejs");
 const mongoose = require('mongoose');
 const md5 = require("md5");
+const bcrypt = require('bcrypt');
 require('dotenv').config();
 
 const app = express();
 const PORT = 3000;
+const saltRounds = 10;
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
@@ -53,33 +55,39 @@ app.get("/logout", (req, res) =>{
 });
 
 app.post("/register", (req, res)=>{
-
-  const newUser = new User({
-    email: req.body.username,
-    password: md5(req.body.password)
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
+  
+    newUser.save()
+    .then(function(){
+        res.render("secrets");
+    })
+    .catch(function(err) {
+        console.log(err);
+      });
   });
 
-  newUser.save()
-  .then(function(){
-      res.render("secrets");
-  })
-  .catch(function(err) {
-      console.log(err);
-    });
 });
 
 
 app.post("/login", (req, res)=>{
 
   const username = req.body.username;
-  const password = md5(req.body.password);
+  const password = req.body.password;
 
   User.findOne({email: username})
   .then(function(foundUser){
-    if(foundUser.password === password){
-      res.render("secrets");
-    }else{
-      res.render("login");
+    if(foundUser){
+      bcrypt.compare(password, foundUser.password, function(err, result) {
+        if(result === true){
+          res.render("secrets");
+        }else{
+          res.render("login");
+        }
+    });
     }
 })
 .catch(function(err) {
