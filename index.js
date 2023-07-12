ea41fd39a29ecc123b811b522410647c181ea3a9
Adrabob <arda_kaya.fb@hotmail.com>
2023-07-12 14:40:10 +0300

    Hash Update
    Signed-off-by: Adrabob <arda_kaya.fb@hotmail.com>

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs =require("ejs");
 const mongoose = require('mongoose');
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 require('dotenv').config();
 
 const app = express();
@@ -31,8 +31,6 @@ const userSchema = new mongoose.Schema({
 });
 
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
-
 const User = mongoose.model("User", userSchema);
 
 
@@ -58,7 +56,7 @@ app.post("/register", (req, res)=>{
 
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
 
   newUser.save()
@@ -74,7 +72,7 @@ app.post("/register", (req, res)=>{
 app.post("/login", (req, res)=>{
 
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({email: username})
   .then(function(foundUser){
@@ -89,8 +87,6 @@ app.post("/login", (req, res)=>{
   });
 
 });
-
-
 
 
 connectDB().then(() =>{
